39999dd74c288b75654677bced0f1ee566b37be4
mikejpoole <mike@yachting.org>
2021-02-23 15:04:21 +0000

    Added Noto Sans JP font and styled browse

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-browse',
-  templateUrl: './browse.component.html',
-  styleUrls: ['./browse.component.scss']
+  templateUrl: './browse.component.html'
 })
 export class BrowseComponent implements OnInit {
 
