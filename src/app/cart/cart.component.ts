df3a206b6c7c215b258d1c26ba77787ff2195c62
mikejpoole <mike@yachting.org>
2021-03-30 15:57:28 +0100

    Started adding checkout form to cart page

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -5,6 +5,7 @@ import { CartService } from '../services/cart.service';
 
 import { Product } from '../models/product.model';
 import { ProductService } from '../services/product.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -14,15 +15,24 @@ import { ProductService } from '../services/product.service';
 export class CartComponent implements OnInit {
   public cart: Cart;
   public items: Product[];
+  form: FormGroup;
 
   constructor(
     public cartService: CartService,
     private productService: ProductService
-  ) { }
+  ) {
+    this.form = new FormGroup({
+      firstname: new FormControl()
+    });
+   }
 
   ngOnInit(): void {
     this.cart = this.cartService.getCart();
     this.items = this.cart.products;
+  }
+
+  checkout(): void{
+    console.log('Checking out...');
   }
 
 }
