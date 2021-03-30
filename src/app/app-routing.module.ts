e4f8ba2324729afa6745b84e349f1f65e3719f62
mikejpoole <mike@yachting.org>
2021-03-30 11:56:33 +0100

    Added cart.component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BrowseComponent } from './browse/browse.component';
+import { CartComponent } from './cart/cart.component';
 import { ProductComponent } from './product/product.component';
 
 const routes: Routes = [
   {path: '', component: BrowseComponent},
+  {path: 'cart', component: CartComponent},
   {path: 'product/:id', component: ProductComponent},
 ];
 
---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Cart } from '../models/cart.model';
+import { CartService } from '../services/cart.service';
+
+import { Product } from '../models/product.model';
+import { ProductService } from '../services/product.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html'
+})
+
+export class CartComponent implements OnInit {
+  public cart: Cart;
+  public items: Product[];
+
+  constructor(
+    private cartService: CartService,
+    private productService: ProductService
+  ) { }
+
+  ngOnInit(): void {
+    this.cart = this.cartService.getCart();
+    this.items = this.cart.products;
+  }
+
+}
