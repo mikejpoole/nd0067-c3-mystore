8ed1813cfb0d67dd50d9564a467833320bbb4bf4
mikejpoole <mike@yachting.org>
2021-03-30 12:58:12 +0100

    Added quantity badge on cart icon in header

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ProductService } from '../services/product.service';
+import { CartService } from '../services/cart.service';
+import { Cart } from '../models/cart.model';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent implements OnInit {
+  public cart: Cart;
 
-  constructor() { }
+  constructor(
+    private cartService: CartService
+  ){ }
 
   ngOnInit(): void {
+    this.cart = this.cartService.getCart();   // This logic should be shared between components
   }
 
 }
