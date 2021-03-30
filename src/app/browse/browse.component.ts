b7159730a1e3a19e816e2883168b84b2d18d9621
mikejpoole <mike@yachting.org>
2021-03-30 05:11:37 +0100

    Displaying products in table on browse page (will probably change this to use grid)

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+// import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+
+import { Product } from '../models/product.model';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-browse',
@@ -6,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BrowseComponent implements OnInit {
 
-  constructor() { }
+  products: Product[] = [
+    // {id: 1, name: 'Foo', price: 5.99},
+    // {id: 2, name: 'Bar', price: 7.99}
+  ];
+
+  constructor(
+    // private router: Router,
+    // private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
 
   ngOnInit(): void {
-  }
+    this.productService.getProducts().subscribe(products => {
+      this.products = products;
+    });
 
+    console.log(this.products);
+  }
 }
