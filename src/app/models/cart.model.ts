16b1a75325b5ba87cf1c9049359ae40fc1100bc3
mikejpoole <mike@yachting.org>
2021-03-31 05:47:14 +0100

    Added customer to cart.model

--- src/app/models/cart.model.ts
+++ src/app/models/cart.model.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 
 import { Product } from '../models/product.model';
+import { User } from './user.model';
 
 export interface Cart {
     products: Product[];
     totalQuantity: number;
     totalPrice: number;
+    customer?: User;
 }
 
 export class ActiveCart implements Cart {
