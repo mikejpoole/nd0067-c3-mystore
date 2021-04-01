52c31ddc89874604a78e1668ae2a13dd473679f2
mikejpoole <mike@yachting.org>
2021-04-01 09:31:47 +0100

    Made the checkoutform.component a child of the cart.component and moved the logic

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 import { Cart } from '../models/cart.model';
 import { CartService } from '../services/cart.service';
@@ -23,7 +23,7 @@ export class CartComponent implements OnInit {
 
   constructor(
     public cartService: CartService,
-    private router: Router,
+
     private route: ActivatedRoute
   ) {}
 
@@ -40,41 +40,11 @@ export class CartComponent implements OnInit {
     if (this.cart.customer) { this.user = this.cart.customer; }
 
     if (!this.orderComplete) {
-      this.form = new FormGroup({
-        firstname: new FormControl(this.user.firstname, [Validators.required, Validators.minLength(2)]),
-        surname: new FormControl(this.user.surname, [Validators.required, Validators.minLength(2)]),
-        email: new FormControl(this.user.email, [Validators.required]),
-        addressLine1: new FormControl(this.user.addressLine1, [Validators.required]),
-        addressLine2: new FormControl(this.user.addressLine2),
-        addressLine3: new FormControl(this.user.addressLine3),
-        town: new FormControl(this.user.town, [Validators.required]),
-        region: new FormControl(this.user.region),
-        postcode: new FormControl(this.user.postcode, [Validators.required])
-      });
+
     }
 
     if (this.orderComplete) {
       this.cart = this.cartService.getEmptyCart();
-    }
-  }
-
-  checkout(): void{
-    // console.log('Checking out...', this.form.value.firstname);
-
-    if (this.cart.customer) {
-      this.cart.customer.firstname    = this.form.value.firstname;
-      this.cart.customer.surname      = this.form.value.surname;
-      this.cart.customer.email        = this.form.value.email;
-      this.cart.customer.addressLine1 = this.form.value.addressLine1;
-      this.cart.customer.addressLine2 = this.form.value.addressLine2;
-      this.cart.customer.addressLine3 = this.form.value.addressLine3;
-      this.cart.customer.town         = this.form.value.town;
-      this.cart.customer.region       = this.form.value.region;
-      this.cart.customer.postcode     = this.form.value.postcode;
-
-      console.log(this.cart.customer);
-
-      this.router.navigate(['/cart/success']);
     }
   }
 }
