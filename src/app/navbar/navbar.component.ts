8961dbf942cabba52cba9541a41e91e1c2cafcda
mikejpoole <mike@yachting.org>
2021-04-01 09:49:05 +0100

    Moved the navbar.component back into the app component because was specifically told that the @Input child page had to be used on the cart.component rather than the navbar.component being a child

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Cart } from '../models/cart.model';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent implements OnInit {
-  // public cart: Cart;
-  @Input() totalQuantity: any;
+  public cart: Cart;
 
   constructor(
+    public cartService: CartService
   ){ }
 
   ngOnInit(): void {
-    // this.cart = this.cartService.getCart();   // Removed because using @Input decorator to get from parent instead
+    this.cart = this.cartService.getCart();
   }
-
 }
